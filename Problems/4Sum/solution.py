d35d4eeef21e29704bb87f43b2fae9d25bd54cf9
Harshit M <79850332+djharshit@users.noreply.github.com>
2022-10-15 15:14:35 +0530

    Create solution.py

---
+++ Problems/4Sum/solution.py
@@ -0,0 +1,20 @@
+import collections
+
+class Solution:
+    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+        nums, result, lookup = sorted(nums), [], collections.defaultdict(list)
+        for i in xrange(0, len(nums) - 1):
+            for j in xrange(i + 1, len(nums)):
+                lookup[nums[i] + nums[j]].append([i, j])
+
+        for i in lookup.keys():
+            if target - i in lookup:
+                for x in lookup[i]:
+                    for y in lookup[target - i]:
+                        [a, b], [c, d] = x, y
+                        if a is not c and a is not d and \
+                           b is not c and b is not d:
+                            quad = sorted([nums[a], nums[b], nums[c], nums[d]])
+                            if quad not in result:
+                                result.append(quad)
+        return sorted(result)
