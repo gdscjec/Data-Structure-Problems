2239280fc5f7ab89a03d3b255f7ef70324dfc1c0
Google Developer Student Club JEC <115336086+gdscjec@users.noreply.github.com>
2022-10-17 11:30:43 +0530

    Edit the code

--- Problems/4Sum/solution.py
+++ Problems/4Sum/solution.py
@@ -2,19 +2,21 @@ import collections
 
 class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
-        nums, result, lookup = sorted(nums), [], collections.defaultdict(list)
-        for i in xrange(0, len(nums) - 1):
-            for j in xrange(i + 1, len(nums)):
-                lookup[nums[i] + nums[j]].append([i, j])
-
-        for i in lookup.keys():
-            if target - i in lookup:
-                for x in lookup[i]:
-                    for y in lookup[target - i]:
-                        [a, b], [c, d] = x, y
-                        if a is not c and a is not d and \
-                           b is not c and b is not d:
-                            quad = sorted([nums[a], nums[b], nums[c], nums[d]])
-                            if quad not in result:
-                                result.append(quad)
-        return sorted(result)
+       res, n = [], len(nums)
+        nums.sort()
+        for a in range(n):
+            for b in range(a+1, n):
+                c = b+1; d = n-1
+                while c<d:
+                    sums = nums[a]+nums[b]+nums[c]+nums[d]
+                    if sums < target:
+                        c += 1
+                    elif sums > target:
+                        d -= 1
+                    else:
+                        toappend = [nums[a],nums[b],nums[c],nums[d]]
+                        if toappend not in res:
+                            res.append(toappend)
+                        c +=1
+                        d-=1
+        return res
