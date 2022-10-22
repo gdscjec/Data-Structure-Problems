3ed0d6ab4d16b2f7bb7a781632b349838a95ece8
Harshit M <79850332+djharshit@users.noreply.github.com>
2022-10-22 12:08:52 +0530

    Update the code - Time limit exceeded error

--- Problems/Largest-Number/solution.py
+++ Problems/Largest-Number/solution.py
@@ -1,10 +1,11 @@
-import itertools
-newls = list(map(int, input().split()))
-
-result = []
-
-for permutation in itertools.permutations(str(number) for number in newls):
-   result.append(''.join(permutation))
-
-maximum = max(result, key=int)
-print(str(maximum))
+class Solution:
+    def largestNumber(self, nums: List[int]) -> str:
+        for i,n in enumerate(nums):
+            nums[i]=str(n)
+        def comparison(n,m):
+            if n+m>m+n:
+                return -1
+            else:
+                return 1
+        nums=sorted(nums, key=cmp_to_key(comparison))
+        return str(int("".join(nums)))
