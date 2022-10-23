6a71c7c7be595c8cbdeeee1e6903d09d88ba9dfa
ishita707 <88577359+Ishitamodi03@users.noreply.github.com>
2022-10-23 12:38:48 +0530

    Program to evaluate reverse polish notation.

---
+++ Problems/Evaluate-Reverse-Polish-Notation/reverse_polish.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+#include<cmath>
+#include<stack>
+#include<climits>
+using namespace std;
+float scanNum(char ch){
+   int value;
+   value = ch;
+   return float(value-'0');
+int isOperator(char ch){
+   if(ch == '+'|| ch == '-'|| ch == '*'|| ch == '/' || ch == '^')
+      return 1;
+   return -1;
+}
+int isOperand(char ch){
+   if(ch >= '0' && ch <= '9')
+      return 1;
+   return -1;
+}
+float operation(int a, int b, char op){
+   
+   if(op == '+')
+      return b+a;
+   else if(op == '-')
+      return b-a;
+   else if(op == '*')
+      return b*a;
+   else if(op == '/')
+      return b/a;
+   else if(op == '^')
+      return pow(b,a);
+   else
+      return INT_MIN;
+}
+float postfixEval(string postfix){
+   int a, b;
+   stack<float> stk;
+   string::iterator it;
+   for(it=postfix.begin(); it!=postfix.end(); it++){
+    
+      if(isOperator(*it) != -1){
+         a = stk.top();
+         stk.pop();
+         b = stk.top();
+         stk.pop();
+         stk.push(operation(a, b, *it));
+      }
+      else if(isOperand(*it) > 0){
+         stk.push(scanNum(*it));
+      }
+   }
+   return stk.top();
+}
+main(){
+   string post = "53+62/*35*+";
+   cout << "The result is: "<<postfixEval(post);
+}
